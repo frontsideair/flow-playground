a4d45cf001c07b88f1ab7a000696af5ff29717d6
Fatih Altinok <photonia@gmail.com>
2018-06-10 20:18:15 +0300

    Iron out bugs

--- maybe.js
+++ maybe.js
@@ -11,7 +11,8 @@ function just<A>(x: A): Maybe<A> {
 }
 
 // const nothing: Maybe<*> = (f, g) => g;
-function nothing(): Maybe<*> {
+// function nothing(): Maybe<*> {
+function nothing<A>(): Maybe<A> {
   return (f, g) => g();
 }
 
@@ -57,12 +58,14 @@ function toString<A: ToString>(fa: Maybe<A>): string {
 }
 
 const _just: Maybe<number> = just(10);
-const _nothing: Maybe<*> = nothing;
+const _nothing: Maybe<*> = nothing();
 
 console.log(toString(_just), toString(_nothing));
 
-console.log(toString(map(x => x + 5)(_just)));
-console.log(toString(map(x => x + 5)(_nothing)));
+console.log(
+  toString(map(x => x + 5)(_just)),
+  toString(map(x => x + 5)(_nothing))
+);
 
 console.log(
   toString(ap(just(a => a + 1))(just(3))),
@@ -74,7 +77,7 @@ console.log(
 function divBy(a: number): number => Maybe<number> {
   return b => {
     if (a === 0) {
-      return nothing;
+      return nothing();
     } else {
       return just(b / a);
     }
@@ -99,54 +102,3 @@ export default {
   bind,
   eq
 };
-
-// type Nothing = { tag: "nothing" };
-// type Just<T> = { tag: "just", value: T };
-// export type Maybe<T> = Nothing | Just<T>;
-
-// export function nothing() {
-//   return { tag: "nothing" };
-// }
-
-// export function just<T>(x: T): Maybe<T> {
-//   return { tag: "just", value: x };
-// }
-
-// type Branches<T, U> = {
-//   nothing: () => U,
-//   just: T => U
-// };
-
-// export function match<T, U>(branches: Branches<T, U>): (Maybe<T>) => U {
-//   return function(maybe) {
-//     switch (maybe.tag) {
-//       case "nothing":
-//         return branches.nothing();
-//       case "just":
-//         return branches.just(maybe.value);
-//       default:
-//         throw new Error((maybe.tag: empty));
-//     }
-//   };
-// }
-
-// const maybe = {
-//   map: function<T, U>(f: T => U): (Maybe<T>) => Maybe<U> {
-//     return match({
-//       nothing: nothing,
-//       just: x => just(f(x))
-//     });
-//   },
-//   eq: function<T>(a: Maybe<T>, b: Maybe<T>): boolean {
-//     return match({
-//       nothing: () => match({ nothing: () => true, just: k(false) })(b),
-//       just: _a =>
-//         match({
-//           nothing: k(false),
-//           just: _b => _a === _b // if T is primitive
-//         })(b)
-//     })(a);
-//   }
-// };
-
-// export default maybe;
