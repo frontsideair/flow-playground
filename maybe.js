1932a05757caab379855bb4a1b67501937289599
Fatih Altinok <photonia@gmail.com>
2018-06-10 20:03:18 +0300

    Progress

--- maybe.js
+++ maybe.js
@@ -1,89 +1,89 @@
 // @flow
 
-type MaybeT<A, B> = ((A) => B, () => B) => B;
-type Maybe<A> = MaybeT<A, *>;
+type Maybe<A, B = *> = ((A) => B, B) => B;
+// type Maybe<A> = MaybeT<A, *>;
 
 // const just: <A, B>(A) => Maybe<A, B> = x => (f, g) => f(x)
-function just<A, B>(x: A): Maybe<A> {
+function just<A>(x: A): Maybe<A> {
   return (f, g) => f(x);
 }
 
-// const nothing: Maybe<*> = (f, g) => g();
-function nothing<A>(): Maybe<A> {
-  return (f, g) => g();
+const nothing: Maybe<*> = (f, g) => g;
+// function nothing<A>(): Maybe<A> {
+//   return (f, g) => g;
+// }
+
+function isJust(fa: Maybe<*>): boolean {
+  return fa(_ => true, false);
 }
 
-function isJust<A>(fa: Maybe<A>): boolean {
-  return fa(_ => true, () => false);
-}
-
-function isNothing<A>(fa: Maybe<A>): boolean {
-  return fa(_ => false, () => true);
+function isNothing(fa: Maybe<*>): boolean {
+  return fa(_ => false, true);
 }
 
 function map<A, B>(f: A => B): (Maybe<A>) => Maybe<B> {
-  // return (fa: Maybe<A>) => fa((a: A) => just(f(a)), nothing);
-  return function(fa: Maybe<A>): Maybe<B> {
-    return fa(a => just(f(a)), nothing);
-  };
+  return fa => fa(a => just(f(a)), nothing);
+  // return function(fa: Maybe<A>): Maybe<B> {
+  //   return fa(a => just(f(a)), nothing);
+  // };
 }
 
 function eq<A>(fa: Maybe<A>, fb: Maybe<A>): boolean {
-  return fa(
-    a => fb(b => a === b, () => false),
-    () => fb(_ => false, () => true)
-  );
+  return fa(a => fb(b => a === b, false), fb(_ => false, true));
 }
 
 const pure = just;
 
 function ap<A, B>(ff: Maybe<(A) => B>): (Maybe<A>) => Maybe<B> {
-  return function(fa: Maybe<A>) {
-    return ff(f => fa(a => just(f(a)), nothing), nothing);
-  };
+  return fa => ff(f => fa(a => just(f(a)), nothing), nothing);
+  // return function(fa: Maybe<A>) {
+  //   return ff(f => fa(a => just(f(a)), nothing), nothing);
+  // };
 }
 
-function bind<A, B>(fa: Maybe<A>) {
-  return function(f: A => Maybe<B>): Maybe<B> {
-    return fa(a => f(a), nothing);
-  };
+function bind<A, B>(fa: Maybe<A>): ((A) => Maybe<B>) => Maybe<B> {
+  return f => fa(a => f(a), nothing);
+  // return function(f: A => Maybe<B>): Maybe<B> {
+  //   return fa(a => f(a), nothing);
+  // };
 }
 
 interface ToString { +toString: () => string }
 
 function toString<A: ToString>(fa: Maybe<A>): string {
-  return fa(a => `Just ${a.toString()}`, () => "Nothing");
+  return fa(a => `Just ${a.toString()}`, "Nothing");
 }
 
 const _just: Maybe<number> = just(10);
-const _nothing: Maybe<number> = nothing();
+const _nothing: Maybe<*> = nothing;
 
 console.log(toString(_just), toString(_nothing));
 
 console.log(toString(map(x => x + 5)(_just)));
+console.log(toString(map(x => x + 5)(_nothing)));
 
 console.log(
   toString(ap(just(a => a + 1))(just(3))),
-  toString(ap(just(a => a + 1))(nothing())),
-  toString(ap(nothing())(just(3))),
-  toString(ap(nothing())(nothing()))
+  toString(ap(just(a => a + 1))(nothing)),
+  toString(ap(nothing)(just(3))),
+  toString(ap(nothing)(nothing))
 );
 
-function divBy(a: number) {
-  return function(b: number): Maybe<number> {
-    if (a === 0) {
-      return nothing();
-    } else {
-      return just(b / a);
-    }
-  };
-}
+// function divBy(a: number): number => Maybe<number> {
+//   return b => {
+//     if (a === 0) {
+//       return nothing;
+//     } else {
+//       return just(b / a);
+//     }
+//   };
+// }
 
 // console.log(
 //   toString(bind(just(3))(divBy(0))),
 //   toString(bind(just(3))(divBy(1))),
-//   toString(bind(nothing())(divBy(0))),
-//   toString(bind(nothing())(divBy(1)))
+//   toString(bind(nothing)(divBy(0))),
+//   toString(bind(nothing)(divBy(1)))
 // );
 
 export default {
